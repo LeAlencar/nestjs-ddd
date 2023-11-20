e44add09e8c20e81827890331d378d98fe72fef9
Leandro Alencar <zzleandrobritozz@gmail.com>
2023-11-19 23:50:13 -0300

    refacotr auth repository

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -6,15 +6,23 @@ import { FetchRecentQuestionsController } from '@/infra/http/controllers/fetch-r
 import { DatabaseModule } from '../database/database.module';
 import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question';
 import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions';
+import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student';
+import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases/authenticate-student';
+import { CriptographyModule } from '../cryptography/cryptography.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, CriptographyModule],
   controllers: [
     AuthenticateController,
     CreateAccountController,
     CreateQuestionController,
     FetchRecentQuestionsController,
   ],
-  providers: [CreateQuestionUseCase, FetchRecentQuestionsUseCase],
+  providers: [
+    CreateQuestionUseCase,
+    FetchRecentQuestionsUseCase,
+    RegisterStudentUseCase,
+    AuthenticateStudentUseCase,
+  ],
 })
 export class HttpModule {}
